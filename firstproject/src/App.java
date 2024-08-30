80829dccb5775a39bdd750f82d33342d8bdbf422
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-08-30 20:43:51 +0000

    edited things

--- firstproject/src/App.java
+++ firstproject/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(mathMagic(5));
+    }
+    public static int mathMagic(int num) {
+        int temp = num;
+        num *= 2;
+        System.out.println(num);
+        num += 6;
+        System.out.println(num);
+        num /= 2;
+        System.out.println(num);
+        num -= temp;
+        System.out.println("final result: " + num);
+        return num;
     }
 }
