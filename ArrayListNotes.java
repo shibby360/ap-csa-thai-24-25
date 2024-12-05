929762418209b511e724b4cd9578b7aaf2d4d8da
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-12-04 20:39:11 -0800

    dec 4, 2024: arraylist practice

--- ArrayListNotes.java
+++ ArrayListNotes.java
@@ -2,6 +2,25 @@ import java.util.ArrayList; // YOU HAVE TO IMPORT ARRAYLIST TO USE IT
 
 public class ArrayListNotes {
     public static void main(String[] args) {
+        warmUpDec4();
+    }
+    public static void warmUpDec4() {
+        ArrayList<String> strList = new ArrayList<String>();
+        ArrayList<Integer> intList = new ArrayList<Integer>(); // for arraylist primitive types(int, double, etc.) use wrapper class(Integer, Double, etc..)
+        for(int i = 1; i <= 10; i++) {
+            intList.add(i);
+        }
+        System.out.println(intList);
+        for(int j = 0; j < 10; j++) {
+            for(int i = 0; i < intList.size(); i++) {
+                if(intList.get(i) < 5 || intList.get(i) == 8) {
+                    intList.remove(i);
+                }
+            }
+        }
+        System.out.println(intList);
+    }
+    public static void notes() {
         ArrayList<String> list = new ArrayList<String>();
         list.add("gay"); // add element
         list.remove(0); // removes index from list
